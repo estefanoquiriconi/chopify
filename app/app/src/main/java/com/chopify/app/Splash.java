ee9f3da5e25133a16b95ceab072af82eefe56ad2
gonzaloDOM39 <gonzaloemanuel39@gmail.com>
2024-11-02 20:25:03 -0300

    sceens splash and login ok

--- app/app/src/main/java/com/chopify/app/Splash.java
+++ app/app/src/main/java/com/chopify/app/Splash.java
@@ -1,12 +1,17 @@
 package com.chopify.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
 
 public class Splash extends AppCompatActivity {
 
@@ -18,6 +23,19 @@ public class Splash extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }, 3000);
+
             return insets;
         });
     }
