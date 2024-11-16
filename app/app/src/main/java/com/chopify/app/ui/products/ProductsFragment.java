70e043256c5f6fc31694a7bede435850654cd9d6
martin <martin@noExiste.com>
2024-11-16 11:04:47 -0300

    search product ok

--- app/app/src/main/java/com/chopify/app/ui/products/ProductsFragment.java
+++ app/app/src/main/java/com/chopify/app/ui/products/ProductsFragment.java
@@ -1,6 +1,9 @@
 package com.chopify.app.ui.products;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +20,11 @@ import java.util.ArrayList;
 public class ProductsFragment extends Fragment {
 
     private FragmentProductsBinding binding;
-
     private ProductAdapter adapter;
+    private ProductsViewModel productsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ProductsViewModel productsViewModel = new ViewModelProvider(this).get(ProductsViewModel.class);
+        productsViewModel = new ViewModelProvider(this).get(ProductsViewModel.class);
 
         binding = FragmentProductsBinding.inflate(inflater, container, false);
         adapter = new ProductAdapter(getContext(), new ArrayList<>());
@@ -29,6 +32,24 @@ public class ProductsFragment extends Fragment {
         binding.rvProductos.setAdapter(adapter);
 
         productsViewModel.getProducts().observe(getViewLifecycleOwner(), products -> adapter.updateProducts(products));
+
+        binding.etBuscarProductos.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                productsViewModel.filterProducts(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        productsViewModel.getFilteredProducts().observe(getViewLifecycleOwner(), products -> {
+            Log.d("ProductsFragment", "Actualizando productos en RecyclerView: " + products.size());
+            adapter.updateProducts(products);
+        });
 
         return binding.getRoot();
     }
--- app/app/src/main/java/com/chopify/app/ui/products/ProductsViewModel.java
+++ app/app/src/main/java/com/chopify/app/ui/products/ProductsViewModel.java
@@ -1,26 +1,57 @@
 package com.chopify.app.ui.products;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.chopify.app.data.entities.Product;
 import com.chopify.app.helpers.SessionManager;
 import com.chopify.app.repositories.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsViewModel extends AndroidViewModel {
 
     private final ProductRepository productRepository;
-
+    private final LiveData<List<Product>> allProducts;
+    private final MutableLiveData<List<Product>> filteredProducts = new MutableLiveData<>();
+    private List<Product> productList = new ArrayList<>();
     private final SessionManager sessionManager;
 
     public ProductsViewModel(Application application) {
         super(application);
         this.productRepository = new ProductRepository(application);
         this.sessionManager = new SessionManager(application.getApplicationContext());
+
+        allProducts = productRepository.getAvailableByBusinessId(sessionManager.getBusiness().getId());
+        allProducts.observeForever(products -> {
+            Log.d("ProductsViewModel", "ProductsViewModel: productos cargados  size=" + products.size());
+            productList = products;
+            filteredProducts.setValue(productList);
+        });
+    }
+
+    public LiveData<List<Product>> getFilteredProducts() {
+        return filteredProducts;
+    }
+
+    public void filterProducts(String query) {
+        Log.d("ProductsViewModel", "filterProducts: filtrado productos por query="+query);
+        if (query == null || query.isEmpty()) {
+            filteredProducts.setValue(productList);
+        } else {
+            List<Product> filteredList = new ArrayList<>();
+            for (Product product : productList) {
+                if (product.getName().toLowerCase().contains(query.toLowerCase())) {
+                    filteredList.add(product);
+                }
+            }
+            filteredProducts.setValue(filteredList);
+        }
     }
 
     public LiveData<List<Product>> getProducts() {
